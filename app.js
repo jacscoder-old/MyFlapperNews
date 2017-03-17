34d0a6d95c26c27dc02423e7696fa551be7babe4
jacscoder <et8fsh@gmail.com>
2017-03-17 10:03:55 +0800

    complete addPost()

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('flapperNews', [])
 	.controller('MainCtrl', [function() {
 		var self = this;
-		self.test = 'Hello world!';
+    
     self.posts = [
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
@@ -9,7 +9,10 @@ var app = angular.module('flapperNews', [])
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
-    self.incrementPost = function() {
+
+    self.addPost = function() {
+      if(!self.title || self.title === '') { return; }
       self.posts.push({title: self.title, upvotes: 4});
+      self.title = '';
     };
 	}]);
