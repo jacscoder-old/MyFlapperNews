0ba4039a93b486c6763a9b61954b806a7ea156d5
jacscoder <et8fsh@gmail.com>
2017-03-17 11:47:02 +0800

    ref(factory): move posts from MainCtrl to posts service

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var app = angular.module('flapperNews', [])
-	.controller('MainCtrl', [function() {
+	
+  .factory('posts', [function() {
+    var data = {
+      posts: [
+        {title: 'post 1', link: 'adsf', upvotes: 5},
+        {title: 'post 2', link: 'fasdf', upvotes: 2},
+        {title: 'post 3', link: 'fasd', upvotes: 15},
+        {title: 'post 4', link: '', upvotes: 9},
+        {title: 'post 5', link: '', upvotes: 4}
+      ]
+    }
+
+    return data
+  }])
+
+  .controller('MainCtrl', ['posts', function(posts) {
 		var self = this;
     
-    self.posts = [
-      {title: 'post 1', link: 'adsf', upvotes: 5},
-      {title: 'post 2', link: 'fasdf', upvotes: 2},
-      {title: 'post 3', link: 'fasd', upvotes: 15},
-      {title: 'post 4', link: '', upvotes: 9},
-      {title: 'post 5', link: '', upvotes: 4}
-    ];
+    self.posts = posts.posts;
 
     self.addPost = function() {
       if(!self.title || self.title === '') { return; }
